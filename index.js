27be4eddea50940acb1fc31019c3e0da47280f8b
Spencer Rafada <spencer.byui@gmail.com>
2022-11-13 10:56:42 -0700

    added routes, controllers, and db connection

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+// Import Routes here
+import tasksRoutes from "./routes/tasks.js";
 
 const app = express();
 app.use(cors());
+dotenv.config();
 
 // Middleware to match the requests Content Type Header and the type
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/tasks", tasksRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+mongoose
+  // Establish connection with Database
+  .connect(process.env.MONGO_URL)
+  // Once connected to the database, run the server on port
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server is running on ${process.env.PORT}`)
+    );
+  })
+  // Display Errors
+  .catch((error) => {
+    console.log(error.message);
+  });
